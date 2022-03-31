5755f8b11113555213b6ee651b338414ddb65a79
chytrolean <84605452+chytrolean@users.noreply.github.com>
2022-03-31 09:54:13 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            list: [
+            {
+                name:'Filip Lojka'
+            },
+            {
+                name:'Filip Haruda'
+            },
+            {
+                name:'Milan Fiala'
+            },
+            {
+                name:'Pavel Frejlich'
+            },
+            {
+                name:'Michal Němeček'
+            },
+            {
+                name:'Kryštof Ešner'
+            },
+            {
+                name:'Michal Drda'
+            },
+            {
+                name:'Jiří Dobeš'
+            },
+            {
+                name:'Daniel Herejk'
+            },
+        ]
+        }
+    }
+})
+app.mount('#app')
